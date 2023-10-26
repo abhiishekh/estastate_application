13f0738af9504c38069f4107d9815e6390c33203
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2023-10-26 21:10:29 +0530

    Created pages and routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
+import Home from './pages/Home'
 
-const App = () => {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/Signin' element={<Signin/>}/>
+    <Route path='/Signup' element={<Signup/>}/>
+    <Route path='/Profile' element={<Profile/>}/>
+    <Route path='/About' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+  
 }
 
-export default App
