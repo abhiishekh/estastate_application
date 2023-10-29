af4b8677f9b72e958e423fb94e312841e77a856a
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2023-10-29 11:33:08 +0530

    connection with the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
+
+// require('dotenv').config();
+import { config } from 'dotenv';
+
 import express from 'express';
 
+import mongoose from 'mongoose';
 
+config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to te db');
+}).catch((err)=>{
+    console.log(err);
+})
+;
+// mongoose.connect("mongodb+srv://abhishek:abhishek@123@mern-estate.q3cjuuo.mongodb.net/mern-estate?retryWrites=true&w=majority")
 
 const app = express();
 
